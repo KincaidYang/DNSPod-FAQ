c26ce9141ddf7c3f3a53f64fae1b9bafc0a484ef
KincaidYang <yjz@r2wind.org>
2022-11-18 21:19:21 +0800

    Update domain
    增加域名部分文档

--- docs/intro.md
+++ docs/intro.md
@@ -1,47 +1,24 @@
 ---
+description: DNSPod FAQ介绍
 sidebar_position: 1
+slug: /
 ---
 
-# Tutorial Intro
+# 介绍
 
-Let's discover **Docusaurus in less than 5 minutes**.
+一分钟快速了解DNSPod FAQ
 
-## Getting Started
+## 快速开始
 
-Get started by **creating a new site**.
+DNSPod FAQ涵盖了 DNSPod 相关产品的常见问题，如域名、DNS 解析、SSL证书、备案、网站建设等。
 
-Or **try Docusaurus immediately** with **[docusaurus.new](https://docusaurus.new)**.
+## 关于 DNSPod FAQ
+DNSPod FAQ 是一个半官方项目，皆在解决广大 DNSPod 用户在使用相关产品过程中遇到的问题。我们希望通过这个项目，能够帮助到更多的用户快速的解决问题，提升用户的使用体验。
+## 如何参与
+我们欢迎任何人参与到 DNSPod FAQ 的维护中来，你可以通过以下方式参与到 DNSPod FAQ 的维护中来：
 
-### What you'll need
+GitHub：您可通过 [GitHub 仓库](https://github.com/KincaidYang/DNSPod-FAQ)上提交 Issue 或 Pull Request 参与维护。
 
-- [Node.js](https://nodejs.org/en/download/) version 16.14 or above:
-  - When installing Node.js, you are recommended to check all checkboxes related to dependencies.
+微信群：您可通过[DNSPod 官方微信群](https://qcloudimg.tencent-cloud.cn/raw/37d34094aa83df21e14d845b28844bbc.png)与我们联系反馈，我们会及时处理您的问题和建议。
 
-## Generate a new site
-
-Generate a new Docusaurus site using the **classic template**.
-
-The classic template will automatically be added to your project after you run the command:
-
-```bash
-npm init docusaurus@latest my-website classic
-```
-
-You can type this command into Command Prompt, Powershell, Terminal, or any other integrated terminal of your code editor.
-
-The command also installs all necessary dependencies you need to run Docusaurus.
-
-## Start your site
-
-Run the development server:
-
-```bash
-cd my-website
-npm run start
-```
-
-The `cd` command changes the directory you're working with. In order to work with your newly created Docusaurus site, you'll need to navigate the terminal there.
-
-The `npm run start` command builds your website locally and serves it through a development server, ready for you to view at http://localhost:3000/.
-
-Open `docs/intro.md` (this page) and edit some lines: the site **reloads automatically** and displays your changes.
+邮件：您可通过 [开发者邮箱](mailto:yjz@r2wind.org)与我们联系反馈。
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,25 +6,25 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'DNSPod FAQ',
+  tagline: 'DNSPod 相关产品常见问题',
+  url: 'https://smb.wiki',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.jpg',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'KincaidYang', // Usually your GitHub org/user name.
+  projectName: 'DNSPod-FAQ', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
   },
 
   presets: [
@@ -33,19 +33,14 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/KincaidYang/DNSPod-FAQ/tree/main/blog',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -57,21 +52,16 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
+        title: 'DNSPod FAQ',
         items: [
-          {
+          /** {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
+            label: '文档',
+          }, */
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/KincaidYang/DNSPod-FAQ',
             label: 'GitHub',
             position: 'right',
           },
@@ -84,7 +74,7 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: '文档',
                 to: '/docs/intro',
               },
             ],
@@ -115,12 +105,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/KincaidYang/DNSPod-FAQ',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} DNSPod FAQ.`,
       },
       prism: {
         theme: lightCodeTheme,
